1ad2bbfbf3bcd350fcaf0216b1b337c435fd89b3
Nathan Clague <nathanclague@gmail.com>
2022-03-18 16:50:09 -0500

    Initial Commit

---
+++ Shared/DailyCodingProblemsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyCodingProblemsApp.swift
+//  Shared
+//
+//  Created by Nathan Clague on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyCodingProblemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
